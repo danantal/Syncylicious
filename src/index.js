8feefc3fb3cd0b8da5520780b6c4cc41376dddcd
Dan Antal <antaldan95@gmail.com>
2017-05-27 18:46:59 +0300

    Remove unnecessary files, add react bootstrap, prepare for ejection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
